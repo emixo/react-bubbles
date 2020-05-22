b2afb41db2cb2168b41a03682c74db80bafa3414
emilio <ediaz-goico@me.bergen.edu>
2020-05-22 12:11:42 -0400

    finished step 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import BubblePage from "./components/BubblePage"
+import PrivateRoute from "./components/PrivateRoute"
 
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
+  const [bubbles, setBubbles] = useState([])
   return (
     <Router>
       <div className="App">
@@ -13,6 +16,7 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute exact path="/protected" component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+import {useHistory} from "react-router-dom"
+import {axiosWithAuth} from "../utils/axiosWithAuth"
+
+const initialLogin = {
+  username: "",
+  password: ""
+}
 
 const Login = () => {
+  const [loginInputs, setLoginInputs] = useState(initialLogin)
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const captureLogin = (e) => {
+    setLoginInputs({
+      ...loginInputs,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const submitLogin = (e) => {
+    e.preventDefault()
+    axiosWithAuth()
+    .post("/api/login", loginInputs)
+    .then(res => {
+      localStorage.setItem("token", res.data.payload)
+      push("/protected")
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
-    </>
+      <form onSubmit={submitLogin}>
+      <label htmlFor="username">Username:</label>
+        <input 
+        name="username"
+        value={loginInputs.username}
+        onChange={captureLogin}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+        name="password"
+        value={loginInputs.password}
+        onChange={captureLogin}
+        />
+        <button>Login</button>
+      </form>
+    </div>
   );
 };
 
